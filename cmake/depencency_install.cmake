c36e1f347fbbdcc37bbfa039401f89f3a40f0aff
lsmon <lsh714@gmail.com>
2026-02-02 12:39:56 -0800

    reformating and adding more modules to make the studying more complete

--- cmake/depencency_install.cmake
+++ cmake/depencency_install.cmake
@@ -1,14 +1,23 @@
-function(dependency_install NAME CHECK_PATH SCRIPT_NAME VERSION)
-    message(STATUS "python3 ${CMAKE_SOURCE_DIR}/scripts/dependencies/${SCRIPT_NAME} ${CMAKE_SOURCE_DIR} ${VERSION}")
+function(install NAME CHECK_PATH SCRIPT_PATH VERSION)
+    message(STATUS "python3 ${SCRIPT_PATH} ${CMAKE_SOURCE_DIR} ${VERSION}")
     if(NOT EXISTS ${CHECK_PATH})
-        message(STATUS "[INSTALL] ${NAME} not found. Running script: ${SCRIPT_NAME}...")    
+        message(STATUS "[INSTALL] ${NAME} not found. Running script: ${SCRIPT_PATH}...")    
         execute_process(
-            COMMAND python3 "${CMAKE_SOURCE_DIR}/scripts/dependencies/${SCRIPT_NAME}" "${CMAKE_SOURCE_DIR}" "${VERSION}"
+            COMMAND python3 "${CMAKE_SOURCE_DIR}/scripts/dependencies/${SCRIPT_PATH}" "${CMAKE_SOURCE_DIR}" "${VERSION}"
             RESULT_VARIABLE RESULT
         )
         if(NOT RESULT EQUAL 0)
             message(FATAL_ERROR "[ERROR] Failed to install ${NAME}")
         endif()
+    else()
+        message(STATUS "[FOUND] ${NAME} already installed at ${CHECK_PATH}")
+    endif()
+endfunction()
+
+function(dependency_install NAME CHECK_PATH SCRIPT_NAME VERSION)
+    if(NOT EXISTS ${CHECK_PATH})
+        message(STATUS "[INSTALL] ${NAME} not found. Running script: ${SCRIPT_NAME}...")
+        install("${NAME}" "${CMAKE_SOURCE_DIR}/scripts/dependencies/${SCRIPT_NAME}" "${VERSION}")
     else()
         message(STATUS "[FOUND] ${NAME} already installed at ${CHECK_PATH}")
     endif()
